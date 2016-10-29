279eeefa716fbbba15ea4689c0566d4dc0e2b231
Margarita Volodina <margo.himera@gmail.com>
2016-10-29 16:00:57 +0300

    Added label for count of games

--- NewHorizon/ViewController.swift
+++ NewHorizon/ViewController.swift
@@ -8,12 +8,8 @@
 
 import UIKit
 import Alamofire
-import BetterSegmentedControl
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var labelMusic: UILabel!
-    @IBOutlet weak var btnInst: UIButton!
     
     @IBAction func OnNewGameTouch(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "MapView", bundle: nil)
@@ -23,40 +19,8 @@ class ViewController: UIViewController {
         return
     }
     
-    func controlValueChanged(_ value: Int)
-    {
-        print(value)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // TODO: split on methods
-        let music = BetterSegmentedControl(
-            frame: CGRect(x: 232.0, y: 370.0, width: 90.0, height: 20.0),
-            titles: ["НЕТ", "ДА"],
-            index: 1,
-            backgroundColor: UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.00),
-            titleColor: .white,
-            indicatorViewBackgroundColor: UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.00),
-            selectedTitleColor: .white)
-        music.titleFont = UIFont(name: "HelveticaNeue", size: 20.0)!
-        music.selectedTitleFont = UIFont(name: "HelveticaNeue-Medium", size: 20.0)!
-        music.addTarget(self, action: #selector(ViewController.controlValueChanged(_:)), for: .valueChanged)
-        view.addSubview(music)
-        
-        music.translatesAutoresizingMaskIntoConstraints = false
-
-        let leadingConstraint = NSLayoutConstraint(item: labelMusic, attribute: .leading, relatedBy: .equal, toItem: music, attribute: .leading, multiplier: 1.0, constant: 0.0)
-        let bottomConstraint = NSLayoutConstraint(item: music, attribute: .bottom, relatedBy: .equal, toItem: btnInst, attribute: .bottom, multiplier: 1.0, constant: 0.0)
-
-        let widthConstraint = NSLayoutConstraint(item: music, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 90)
-        let heightConstraint = NSLayoutConstraint(item: music, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 30)
-
-        let verticalSpacingConstraint = NSLayoutConstraint(item: labelMusic, attribute: .bottom, relatedBy: .equal, toItem: music, attribute: .top, multiplier: 1.0, constant: -4.0)
-        
-        NSLayoutConstraint.activate([bottomConstraint, leadingConstraint, widthConstraint, heightConstraint, verticalSpacingConstraint])
-        self.view.layoutIfNeeded()
     }
 }
 
